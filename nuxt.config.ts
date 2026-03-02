61a5d01bd4eb031d8b05708d643d54368aca6c8f
Yunho Park <168257476+kr4phy@users.noreply.github.com>
2026-03-02 21:22:09 +0900

    fix prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,6 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    baseURL: /mealmenu/,
     head: {
       title: 'Meal Menu',
       htmlAttrs: {
@@ -28,6 +27,6 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍽️</text></svg>', sizes: 'any'},
       ]
-    },
+    }
   }
 })
